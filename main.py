7d5332a8c72af2b6f4b13491205358ae77040da8
likanug <likanug.g@qq.com>
2025-06-16 23:10:43 +0800

    更新日志输出和readme

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     tencent_data = fetch_qq_cartoon_today()
     data = utils.merge_data(mikanani_data, tencent_data)  # 合并两个数据字典
     update_today_section_in_readme(data)
-    logging.info("今日的 Mikanani 数据已更新到 README 中。")
+    logging.info("今日的番剧更新数据已更新到 README 中。")
     # with open("mikanani_today.json", "w", encoding="utf-8") as f:
     #     json.dump(data, f, ensure_ascii=False, indent=2)
     # print(json.dumps(data, ensure_ascii=False, indent=2))
