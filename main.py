eb6f1cf3f9aebcf35bd4d9d445d6aa2cb600a995
likanug <likanug.g@qq.com>
2025-06-16 16:10:06 +0800

    添加作业执行日志

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from utils import update_today_section_in_readme
 
 def main():
     data = fetch_mikanani_today()
+    print(json.dumps(data, ensure_ascii=False, indent=2))
     update_today_section_in_readme(data)
     # with open("mikanani_today.json", "w", encoding="utf-8") as f:
     #     json.dump(data, f, ensure_ascii=False, indent=2)
