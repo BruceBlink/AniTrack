169a8f9c6fb9fe625dfad8ee3b6ddb9fa60837b4
likanug <likanug.g@qq.com>
2025-06-17 23:50:16 +0800

    重构main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import utils
-from bilibili import fetch_bilibili_guochuang_today, fetch_bilibili_anime_today
+from bilibili import BilibiliFetcher
+from config import BILIBILI_GUOCHUANG_API, BILIBILI_ANIME_API
 from iqiyi import fetch_iqiyi_cartoon_today
 from logger_setup import init_logger
 from mikanani import fetch_mikanani_today
@@ -9,25 +10,39 @@ from utils import update_today_section_in_readme
 
 
 def main():
-    # 统一初始化 logger
-    init_logger(level=logging.DEBUG)
+    init()
     logging.info("开始获取今日的 追番数据...")
     # 获取今日的追番数据
     mikanani_data = fetch_mikanani_today()
     # 获取腾讯动漫今日更新数据
     tencent_data = fetch_qq_cartoon_today()
     # 获取 bilibili 国创和番剧今日更新数据
-    bilibili_guochuang_data = fetch_bilibili_guochuang_today()
-    bilibili_anime_data = fetch_bilibili_anime_today()
+    fetcher = FetcherImpl()
+    bilibili_guochuang_data = fetcher.bilibili_guochuang.fetch_bilibili_cartoon_today()
+    bilibili_anime_data = fetcher.bilibili_guochuang.fetch_bilibili_cartoon_today()
     # 获取iqiyi 今日更新数据
     iqiyi_cartoon_data = fetch_iqiyi_cartoon_today()
-    data = utils.merge_dict_data(mikanani_data, tencent_data, bilibili_guochuang_data, bilibili_anime_data, iqiyi_cartoon_data)  # 合并多个数据字典
+    data = utils.merge_dict_data(mikanani_data, tencent_data, bilibili_guochuang_data, bilibili_anime_data,
+                                 iqiyi_cartoon_data)  # 合并多个数据字典
     # 更新 README 中的今日番剧更新部分
     update_today_section_in_readme(data)
     logging.info("今日的番剧更新数据已更新到 README 中。")
     # with open("mikanani_today.json", "w", encoding="utf-8") as f:
     #     json.dump(data, f, ensure_ascii=False, indent=2)
     # print(json.dumps(data, ensure_ascii=False, indent=2))
+
+
+def init():
+    """初始化日志记录器"""
+    # 统一初始化 logger
+    init_logger(level=logging.DEBUG)
+    logging.info("日志记录器已初始化。")
+
+
+class FetcherImpl:
+    def __init__(self):
+        self.bilibili_guochuang = BilibiliFetcher(BILIBILI_GUOCHUANG_API)
+        self.bilibili_anime = BilibiliFetcher(BILIBILI_ANIME_API)
 
 
 if __name__ == "__main__":
