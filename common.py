60b954ef117e9c6338233b74c40acf0ba41db4df
likanug <likanug.g@qq.com>
2025-06-17 12:01:40 +0800

    定义统一返回结果Result类

---
+++ common.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass(order=True)
+class Result:
+    platform: str
+    title: str
+    update_count: int
+    update_info: str
+    image_url: str
+    detail_url: str
+    update_time: str
