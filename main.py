2890fae1cbafdb168ae07060a92b87dcb386dd0a
likanug <likanug.g@qq.com>
2025-06-17 23:50:16 +0800

    重构main.py

--- main.py
+++ main.py
@@ -12,21 +12,17 @@ from utils import update_today_section_in_readme
 def main():
     init()
     logging.info("开始获取今日的 追番数据...")
-    # 获取蜜柑计划今日的追番数据
+    # 获取今日的追番数据
     mikanani_data = fetch_mikanani_today()
     # 获取腾讯动漫今日更新数据
     tencent_data = fetch_qq_cartoon_today()
     # 获取 bilibili 国创和番剧今日更新数据
     fetcher = FetcherImpl()
     bilibili_guochuang_data = fetcher.bilibili_guochuang.fetch_bilibili_cartoon_today()
-    bilibili_anime_data = fetcher.bilibili_anime.fetch_bilibili_cartoon_today()
+    bilibili_anime_data = fetcher.bilibili_guochuang.fetch_bilibili_cartoon_today()
     # 获取iqiyi 今日更新数据
     iqiyi_cartoon_data = fetch_iqiyi_cartoon_today()
-    # 合并获取的数据
-    data = utils.merge_dict_data(mikanani_data,
-                                 tencent_data,
-                                 bilibili_guochuang_data,
-                                 bilibili_anime_data,
+    data = utils.merge_dict_data(mikanani_data, tencent_data, bilibili_guochuang_data, bilibili_anime_data,
                                  iqiyi_cartoon_data)  # 合并多个数据字典
     # 更新 README 中的今日番剧更新部分
     update_today_section_in_readme(data)
