a685e17ce7d89e82feb42b101cf4481fd2ff1d8a
likanug <likanug.g@qq.com>
2025-07-01 16:55:08 +0800

    fix: remove trailing comma from MIKANANI_BASE_URL in config.py

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@
 # 腾讯动漫
 TENCENT_CARTOON_BASE_URL = "https://v.qq.com/channel/cartoon"
 # 蜜柑计划
-MIKANANI_BASE_URL = "https://mikanani.me",
+MIKANANI_BASE_URL = "https://mikanani.me"
 MIKANANI_BASE_URL_1 =  "https://mikanani.me/Home/BangumiCoverFlowByDayOfWeek?year=2025&seasonStr=%E6%98%A5"
 # Bilibili 国创
 BILIBILI_GUOCHUANG_API = "https://api.bilibili.com/pgc/web/timeline?types=4&before=6&after=6"
